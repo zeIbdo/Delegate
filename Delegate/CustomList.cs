56899bc5fb20c4a15c47f837238656873df42352
zeIbdo <ibrahimrm@code.edu.az>
2024-07-17 03:17:10 +0400

    Add project files.

---
+++ Delegate/CustomList.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+
+namespace Delegate;
+
+public class CustomList<T>:IEnumerable<T>
+{
+    private T[] arr;
+    public CustomList()
+    {
+        arr = new T[0];
+
+    }
+    public void Add(T item)
+    {
+        Array.Resize(ref arr, arr.Length+1);
+        arr[arr.Length - 1] = item;
+    }
+    public T this[int index] { get => arr[index]; set => arr[index] = value;}
+    public bool Remove(T item)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if(arr[i].Equals(item))
+            {
+                for(int j = i; j < arr.Length-1; j++)
+                {
+                    arr[j] = arr[j+1];
+                }
+                Array.Resize(ref arr, arr.Length-1);
+                return true;
+            }
+            else return false;                
+        }
+        return false;
+    }
+    public T? Find(Predicate<T> predicate)
+    {
+        foreach(T item in  arr)
+        {
+            if (predicate(item))
+            {
+                return item;
+            }
+        }
+        return default(T);
+    }
+    public CustomList<T> FindAll(Predicate<T> predicate)
+    {
+        CustomList<T> list = new CustomList<T>();
+        foreach(T item in arr)
+        {
+            if(predicate(item)==true)
+                list.Add(item);
+
+        }
+        return list;
+    }
+    public int RemoveAll(Predicate<T> predicate)
+    {
+        int count = 0;
+        for(int i = 0; i < arr.Length; i++)
+        {
+            if(predicate(arr[i]) == true)
+            {
+                count++;
+                for(int j = i; j < arr.Length - 1; j++)
+                {
+                    arr[j] = arr[j+1];
+                }
+                Array.Resize(ref arr,arr.Length-1);
+            }
+        }
+        return count;
+    }
+    public void Clear()
+    {
+        arr = new T[0];
+    }
+    public int Count
+    {
+        get => arr.Length;
+    }
+    public IEnumerator<T> GetEnumerator()
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            yield return arr[i];
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
---
+++ Delegate/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Delegate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //CustomList<int> ints = new CustomList<int>();
+            //ints.Add(1);
+            //ints.Add(2);
+            //ints.Add(3);
+            //ints.Add(4);
+            //ints.Add(5);
+            //ints.Add(6);
+            //ints.Add(1);
+            //foreach (int i in ints)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //Console.WriteLine("===================================");
+            ////Console.WriteLine( ints.Find(x=>x==1));
+            //Console.WriteLine( ints.RemoveAll(x=>x==1));
+            //Console.WriteLine("===================================");
+            //foreach (int i in ints)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            ////foreach(int i in ints.FindAll(x => x %2== 1))
+            ////{
+            ////    Console.WriteLine(i);
+            ////}
+            //Console.WriteLine("=====================================");
+            ////ints.RemoveAll(x => x / 2 == 0);
+            ////foreach (int i in ints)
+            ////{
+            ////    Console.WriteLine(i);
+            ////}
+            ////List<int> ints2 = new List<int>();
+            ////ints2.Add(1);
+            ////ints2.Add(2);
+            ////ints2.Add(3);
+            ////ints2.Add(4);
+            ////Console.WriteLine( ints2.RemoveAll(ints2 => ints2 %2 == 0));
+        }
+    }
+}
